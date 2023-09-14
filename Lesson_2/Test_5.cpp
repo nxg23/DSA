f1699588671b0f67f533b50419aeb41d59053145
HyperG <130085031+nxg23@users.noreply.github.com>
2023-09-14 16:01:56 +0700

    Add files via upload

--- Lesson_2/Test_5.cpp
+++ Lesson_2/Test_5.cpp
@@ -1,31 +1,82 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
+class Stack
 {
-    stack<int> st ;
-    int T, val;
-    cout << "Input: ";
-    cin >> T;
-    while(T--){
+private:
+public:
+    int size = 100;
+    int top = -1 ;
+    int stack[100];
+    
+    bool isEmpty()
+   {
+    if(top != 0) return false;
+    return true;
+   }
+
+    bool isFull()
+   {
+    if(top != size) return false;
+    return true;
+   }
+
+    void insert( int val )
+    {
+       if(isFull()) cout<< "Ngan xep day khong the chen ";
+     else {
+    top++;
+    stack[top] = val;
+  }
+    }
+
+    void erase()
+    {
+    if(isEmpty())  cout << "Ngan xep rong khong the xoa";
+    else{
+        top--;
+    }
+    }
+
+    void print()
+    {
+      cout << "Phan tu dau ngan xep: " << stack[top] ;
+    }
+
+};
+
+void menu(int T)
+{
+    Stack st;
+    int val;
+      while(T--){
        int chon;
         cout << "Nhap lua chon 0 de chen va 1 de xoa: ";
         cin >> chon ;
         if(chon == 0){
-         cout <<"Value: "; cin >> val;
-         st.push(val);
+            cout << "value: "; cin >> val;
+            st.insert(val);
+            st.print();
+            cout << endl;
         }
-        if(chon == 1) {
-            st.pop();
-        }
-
+        else if(chon == 1){
+            st.erase();
+            st.print();
+            cout <<endl;
+		}
+		else {
+			cin.ignore();
+			cout <<"\n" ;
+			cout << "Nhap sai hay chon lai \n\n";
+			menu(T);
+		}
     }
-    vector<int> v;
-    while(!st.empty()){
-    v.push_back(st.top());
-    st.pop();
-  }
-  
-  for(int i = v.size() - 1; i >= 0; i--) cout << v[i] << " ";
+}
+int main()
+{
+    int T;
+    cout << "Input: ";
+    cin >> T;
+    menu(T);
     return 0;
 }
